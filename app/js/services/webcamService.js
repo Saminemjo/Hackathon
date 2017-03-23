4cca4efa62402f1ca2182f44aa2d66979fc7c908
Sabrina <sabrina.mardjoeki@gmail.com>
2017-03-23 17:27:46 +0100

    api webcam travel

---
+++ app/js/services/webcamService.js
@@ -0,0 +1,14 @@
+angular.module('app')
+    .service('webcamService', function($http) {
+      $http.defaults.headers.common["X-Mashape-Key"] = "gSfaTTja0bmshVgDSAJexFPYhD2Ep1N86fgjsn4liGSxnv8dNx";
+
+        return {
+            getAll: function() {
+                return $http.get('/users');
+            },
+            getOne: function(id) {
+                return $http.get('/users/' + id);
+            },
+
+        };
+    });
